d1738b471b46ed7f785b76007e96a45c045d3a41
kommunistMaou <kommunistmaou@gmail.com>
2019-04-09 22:23:31 +0300

    Changed import PhonesPage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import PhonesPage from './phones/phones-page.js/index.js';
+import PhonesPage from './phones/phones-page.js';
 const phonesPage = new PhonesPage({
     element: document.querySelector('[data-page-container]')
 })
